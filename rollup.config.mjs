89076d1912e263e04f70049d3d68813685d2b0e2
lideming <me@yuuza.net>
2023-01-02 20:41:47 +0800

    feat: add build config

---
+++ rollup.config.mjs
@@ -0,0 +1,19 @@
+import sucrase from "@rollup/plugin-sucrase";
+import resolve from "@rollup/plugin-node-resolve";
+import { readdir } from "fs/promises";
+
+const modules = await readdir("plugins");
+
+/** @type {import("rollup").RollupOptions[]} */
+export default modules.map((m) => ({
+  input: `plugins/${m}/index.ts`,
+  output: {
+    file: `dist/${m}.js`,
+    format: "iife",
+    globals: {
+      "@yuuza/mcloud": "mcloud",
+      "@yuuza/webfx": "mcloud.webfx",
+    },
+  },
+  external: ["@yuuza/mcloud", "@yuuza/webfx"],
+}));
